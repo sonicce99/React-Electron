fb8ad1c06c5e0128538cecd99f95e814c30f10b9
dongsu <sonicce99@naver.com>
2022-12-14 13:53:20 +0900

    feat : ipcRenderer 설정

--- preload.js
+++ preload.js
@@ -4,6 +4,6 @@ contextBridge.exposeInMainWorld("versions", {
   node: () => process.versions.node,
   chrome: () => process.versions.chrome,
   electron: () => process.versions.electron,
-  ping: () => ipcRenderer.invoke("ping"),
+  ping: () => ipcRenderer.invoke("실행됨"),
   // we can also expose variables, not just functions
 });
